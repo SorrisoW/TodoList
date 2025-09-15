1dce716b624ca2438c80281dbad2ebd0e9acc533
SorrisoW <metranca.w.g@gmail.com>
2025-09-15 20:03:12 -0300

    criado manipulacao

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const list = document.querySelector('#todo-list')
+const list = document.querySelector('#todo-list');
 
-const li = document.createElement("li")
+const li = document.createElement("li");
 
 li.textContent = "Estudar com os materiais do ClassRoom"
-list.append(li)
+ list.append(li);
+
+
+list.addEventListener("click", event => {
+    const li = event.target.closest("li")
+
+    if(!li) return
+
+    li.classList.toggle("done")
+});
